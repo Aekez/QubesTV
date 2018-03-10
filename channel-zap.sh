2021175282fcaefe7fe105fcafdbbf596a0a2fcf
Yuraeitha <33869013+Aekez@users.noreply.github.com>
2018-03-10 03:49:06 +0100

    Create channel-zap.sh

---
+++ channel-zap.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+##Please note that this script only changes a stream of an already running firefox window.
+qvm-run QubesTV 'bash -e firefox https://www.youtube.com/watch?v=ChOhcHD8fBA&feature=youtu.be&t=68'
